4cc2f1c3797dee67019dccf3bfe5084246588038
Clayton Prebelli <clayton@prebelli.online>
2021-08-27 12:35:42 -0300

    Inseridos og metas

--- src/app/views/contato/contato.component.ts
+++ src/app/views/contato/contato.component.ts
@@ -14,7 +14,11 @@ export class ContatoComponent implements OnInit {
     this.meta.addTags([
       { name: 'description', content: 'Contato de teste aulas angular' },
       { name: 'title', content: 'Contato' },
-      { name: 'keywords', content: 'angular, javascript, typescript, meta, seo' }  
+      { name: 'keywords', content: 'angular, javascript, typescript, meta, seo' } ,
+      { name: 'og:title', content: 'Contato Teste' },
+      { name: 'og:description', content: 'Contato de teste aulas angular' },
+      { name: 'og:image', content: 'https://image.tmdb.org/t/p/w500/wTS3dS2DJiMFFgqKDz5fxMTri.jpg' },
+      { name: 'og:site_name', content: 'Contato da Aplicação' },
     ]);
   }
 
